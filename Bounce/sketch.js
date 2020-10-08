1c90ec2023ba6c9c6792184c0c4c2b11999ade36
Magnus <magn7340@gmail.comm>
2020-10-08 12:54:31 +0200

    bounce update

--- Bounce/sketch.js
+++ Bounce/sketch.js
@@ -1,4 +1,9 @@
 let cx = 0;
+let hastighed = 2;
+let diameter = 50;
+let cz = 0;
+let cy = 600;
+let cb = 600;
 
 function setup() {
     createCanvas(600, 600);
@@ -6,14 +11,45 @@ function setup() {
 
 function draw() {
     background(220);
-    circle(cx, 300, 50);
-    cx = cx + 2;
+    circle(cx, 300, diameter);
+    circle(300, cz, diameter);
+    // circle(cy, 300, diameter);
+    // circle(300, cb, diameter);
 
-    if (cx = width) {
-        cx = cx - 1
-    } else if (cx < width) {
-        cx = cx + 2
+    if (cx > width - diameter / 2) {
+        hastighed = -2
     }
+    if (cx < 0 + diameter / 2) {
+        hastighed = 2
+    }
+    cx = cx + hastighed
+
+    if (cz > height + diameter / 2) {
+        hastighed = -2
+    }
+    if (cz < 0 - diameter / 2) {
+        hastighed = 2
+    }
+    cz = cz + hastighed
 
 
+
+
+    // if (cy > width - diameter / 2) {
+    //     hastighed = -2
+    // }
+    // if (cy < 0 + diameter / 2) {
+    //     hastighed = 2
+    // }
+    // cy = cy + hastighed
+
+
+
+    // if (cb > height + diameter / 2) {
+    //     hastighed = -2
+    // }
+    // if (cb < 0 - diameter / 2) {
+    //     hastighed = 2
+    // }
+    // cb = cb + hastighed
 }
